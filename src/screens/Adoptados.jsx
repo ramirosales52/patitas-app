0325ae0b6c2ff0fbc8edcf576cc2a7d2d3138341
Ramiro <ramirosales52@gmail.com>
2023-04-18 05:45:03 -0300

    test api

--- src/screens/Adoptados.jsx
+++ src/screens/Adoptados.jsx
@@ -1,11 +1,37 @@
-import React from "react"; 
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react"; 
+import { View, Text, ScrollView, Image, SafeAreaView, StatusBar } from "react-native";
+import axios from "axios";
 
 const Adoptados = () => {
+
+  const [ imagenes, setImagenes ] = useState([])
+
+  useEffect(() => {
+    const getImagenes = async() => {
+        const result = await axios.get('https://api.unsplash.com/photos/?client_id=_rTqr0aFcKi-9GWBav3vrbhoEPe5xCB5RZTAxHnHnfU') 
+            setImagenes(result.data) 
+        }
+        getImagenes()
+    }, []);
+  console.log(imagenes)
+
   return (
-    <View>
-      <Text style={{ fontSize: 30, textAlign: 'center', marginTop: '20%' }}>Adoptados</Text>
-    </View>
+    
+      <ScrollView contentContainerStyle={{justifyContent: 'center', alignItems:'center'}}>
+        {imagenes.map(imagen => [
+          console.log(imagen.urls.small),
+          <View style={{paddingBottom: 10}}>
+            <Image
+              style={{width: 200, height: 200}}
+              source={{ uri: imagen.urls.small }}
+              key={imagen.id}
+            />
+          </View>
+
+        ]
+        )}
+      </ScrollView>
+    
   )
 }
 
